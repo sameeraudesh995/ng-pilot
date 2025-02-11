c6e63bc4c971d376b7fafc0e5d68d9ac1ca58813
sameeraudesh995 <sameeraudesh.2225@gmail.com>
2025-02-12 05:28:10 +0530

    nav-bar added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {HomeComponent} from './components/homepage/homepage.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path:'',redirectTo:'/home', pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  { path: '**', redirectTo: '' },
+
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutes{ }
